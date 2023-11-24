1ccd954218a4450326e82f245b12fbec9af42d52
ISbunny <sonukmr121@gmail.com>
2023-11-24 09:15:01 -0800

    router-view

---
+++ src/components/MyName.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>SONU KUMAR</p>
+  </div>
+</template>
+<script>
+
+
+export default {
+  name: "MyName",
+ 
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import createRouter from './router';
+import Axios from 'axios';
 // import ActiveElement from './components/ActiveElement.vue';
 // import KnowledgeBase from './components/KnowledgeBase.vue';
 // import KnowledgeElement from './components/KnowledgeElement.vue';
 // import KnowledgeGrid from './components/KnowledgeGrid.vue';
 
-createApp(App).mount("#app");
+// createApp(App).mount("#app");
 
 // app.component('active-element', ActiveElement);
 // app.component('knowledge-base', KnowledgeBase);
@@ -14,3 +16,7 @@ createApp(App).mount("#app");
 // app.component('knowledge-grid', KnowledgeGrid);
 
 // app.mount('#app');
+
+const app = createApp(App).use(createRouter());
+app.config.globalProperties.$http = Axios;
+app.mount('#app');
