c2cb41817d414a5b89eac53747333de7021ebdb2
ISbunny <sonukmr121@gmail.com>
2023-11-25 07:02:20 -0800

    basic component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/calc">Calculation</router-link> |
-      <router-link to="/people">People</router-link> |
-      <router-link to="/brew">Breweries</router-link> 
-    </div>
-    <router-view />
+  <div>
+    <TheHeader />
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
   </div>
 </template>
+
 <script>
-
-export default {};
+import TheHeader from './components/TheHeader.vue';
+export default {
+  components: {
+    TheHeader,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
 </script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html {
+  font-family: sans-serif;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0;
 }
 </style>
--- src/views/Calculation.vue
+++
@@ -1,7 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<template>
-    <div>
-        <h1>About</h1>
-        <p>This is the about page</p>
-    </div>
-</template>
