c52426cc1b754e11cde75dcf539412ad8d81325a
ISbunny <sonukmr121@gmail.com>
2023-11-27 08:42:28 -0800

    routing

---
+++ src/components/users/UsersList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <ul>
+      <button @click="confirminput">Confirm</button>
+      <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  methods:{
+    confirminput(){
+      this.$router.push('/teams')
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue';
-
-import BaseCard from './components/UI/BaseCard.vue';
-import BaseButton from './components/UI/BaseButton.vue';
+import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {path:'/teams',component:TeamsList},
+        {path:'/users',component:UsersList},
+        {path:'/teams?id',component:TeamMembers},
+    ],
+    linkActiveClass:'active',
+})
 
-const app = createApp(App);
-
-app.component('base-card', BaseCard);
-app.component('base-button', BaseButton);
-
+const app = createApp(App)
+app.use(router)
 app.mount('#app');
